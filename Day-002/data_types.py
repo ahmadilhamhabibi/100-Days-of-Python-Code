6ab10fb81af4b62e941e127165d2cb5985497bb6
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-10-01 17:25:09 +0700

    Create data_types.py

---
+++ Day-002/data_types.py
@@ -0,0 +1,25 @@
+# Data types
+
+
+# String
+
+print("Hello"[0]) # substring
+
+print("Hello" + " " + "World")
+
+print("123" + "345") # string in double quote
+
+
+# integer
+
+print(123 + 345) # integer
+
+
+# Float
+
+print(3.14)
+
+
+# Boolean
+print(True)
+print(False)
