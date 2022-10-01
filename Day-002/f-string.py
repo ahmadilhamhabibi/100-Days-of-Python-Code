41dbe92894f633eabc9c6e45acf88d1a501aafca
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-10-01 17:25:20 +0700

    Create f-string.py

---
+++ Day-002/f-string.py
@@ -0,0 +1,17 @@
+# Round / Pembulatan
+print(round(10 / 3, 2))
+
+
+# Single / and Double //
+print(type(10 / 3)) # float
+
+print(type(10 // 3)) # int
+
+
+
+# f-String
+
+score = 80
+height = 1.8
+isWinning = True
+print(f"Your score is {score}, your height is {height}, and your winning is {isWinning}")
