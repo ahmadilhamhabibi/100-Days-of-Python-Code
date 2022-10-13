588c849f97dc54318310ed1c78af463c24d42b69
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-10-13 17:19:13 +0700

    Create high-score.py

---
+++ Day-005/high-score.py
@@ -0,0 +1,10 @@
+student_scores = input("Inout a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+high_score = 0
+for score in student_scores:
+    if score > high_score:
+        high_score = score
+print(f"Your high score is {high_score}")
