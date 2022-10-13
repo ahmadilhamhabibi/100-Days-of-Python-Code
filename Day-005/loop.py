0402e5d67575d5d9a2f6ab42fa81722a49cb5cd5
Ahmad Ilham Habibi <ahmadilhamhabibi@gmail.com>
2022-10-13 17:19:06 +0700

    Create loop.py

---
+++ Day-005/loop.py
@@ -0,0 +1,18 @@
+# fruits = ["Apple", "Peach", "Pear"]
+# for i in fruits:
+#     print(i)
+#     print(i + " Pie")
+
+
+# # Loop in range
+# total = 0
+# for number in range(1, 101):
+#     total += number
+# print(total)
+
+
+# Looping even number
+total_even = 0
+for even in range(2, 101, 2):
+    total_even += even
+print(total_even)
